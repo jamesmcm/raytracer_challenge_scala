a9b1ab33eda38554f8c376b1d9dde0e4a46f92e1
James McMurray <jamesmcm03@gmail.com>
2019-12-05 02:23:00 +0100

    Cone WIP

---
+++ src/main/scala/raytracer/Cone.scala
@@ -0,0 +1,123 @@
+// Copyright (C) 2019 James McMurray
+//
+// raytracer_challenge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// raytracer_challenge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with raytracer_challenge  If not, see <http://www.gnu.org/licenses/>.
+
+package raytracer
+
+import cats.implicits._
+
+class Cone(val transform: Matrix,
+           val material: Material,
+           val minimum: Double,
+           val maximum: Double,
+           val closed: Boolean)
+    extends SpaceObject {
+  type T = Cone
+
+  def constructor(t: Matrix, m: Material): T =
+    new Cone(t, m, Double.NegativeInfinity, Double.PositiveInfinity, false)
+
+  final override def equals(that: Any): Boolean = {
+    that match {
+      case that: Cone => transform === that.transform && material === that.material
+      case _          => false
+    }
+  }
+
+  final override def hashCode: Int = (transform, material).##
+
+  def setMinimum(x: Double): Cone = {
+    new Cone(transform, material, x, maximum, closed)
+  }
+  def setMaximum(x: Double): Cone = {
+    new Cone(transform, material, minimum, x, closed)
+  }
+  def setClosed(x: Boolean): Cone = {
+    new Cone(transform, material, minimum, maximum, x)
+  }
+
+  def intersectCaps(r: Ray): Seq[Intersection] = {
+    if ((!closed) || doubleEq(r.direction.y, 0)) List()
+    else {
+
+      ((minimum - r.origin.y) / r.direction.y, (maximum - r.origin.y) / r.direction.y) match {
+        case (a, b) if Cone.checkCap(r, a, minimum) && Cone.checkCap(r, b, maximum) =>
+          List(Intersection(a, this), Intersection(b, this))
+        case (a, b) if Cone.checkCap(r, a, minimum) => List(Intersection(a, this))
+        case (a, b) if Cone.checkCap(r, b, maximum) => List(Intersection(b, this))
+        case _                             => List()
+      }
+    }
+  }
+
+  def localIntersect(r: Ray): Seq[Intersection] = {
+    val a
+      : Double = r.direction.x * r.direction.x - r.direction.y * r.direction.y + r.direction.z * r.direction.z
+    val b
+      : Double    = 2 * r.origin.x * r.direction.x - 2 * r.origin.y * r.direction.y + 2 * r.origin.z * r.direction.z
+    val c: Double = r.origin.x * r.origin.x - r.origin.y * r.origin.y + r.origin.z * r.origin.z - 1
+    (a, b) match {
+      case (x, y) if doubleEq(x, 0) && doubleEq(y, 0) => intersectCaps(r)
+      case (x, _) if doubleEq(x, 0)                   => List(Intersection(-c / (2 * b), this)) ++ intersectCaps(r)
+      case _ => {
+        val disc: Double = (b * b) - 4 * a * c
+        if (disc < 0) intersectCaps(r)
+        else {
+          val t0: Double = (-b - math.sqrt(disc)) / (2 * a)
+          val t1: Double = (-b + math.sqrt(disc)) / (2 * a)
+
+          //TODO: Ensure left is smaller
+          (r.origin.y + t0 * r.direction.y, r.origin.y + t1 * r.direction.y) match {
+            case (a, b) if (a > minimum && a < maximum && b > minimum && b < maximum) =>
+              List(Intersection(t0, this), Intersection(t1, this)) ++ intersectCaps(r)
+            case (a, b) if (a > minimum && a < maximum) =>
+              List(Intersection(t0, this)) ++ intersectCaps(r)
+            case (a, b) if (b > minimum && b < maximum) =>
+              List(Intersection(t1, this)) ++ intersectCaps(r)
+            case _ => intersectCaps(r)
+          }
+        }
+      }
+    }
+  }
+
+  def localNormalAt(p: RTTuple): RTTuple = {
+    val dist: Double = p.x * p.x + p.z * p.z
+
+    if (dist < 1 && p.y >= maximum - EPSILON) Vector(0, 1, 0)
+    else {
+      if (dist < 1 && p.y <= minimum + EPSILON) Vector(0, -1, 0)
+      else {
+        val y: Double = math.sqrt(p.x*p.x + p.z*p.z)
+        Vector(p.x, if (p.y>0) -y else y, p.z)
+      }
+    }
+  }
+}
+
+object Cone {
+  def apply(): Cone =
+    new Cone(Matrix.getIdentityMatrix(4),
+             Material.defaultMaterial(),
+             Double.NegativeInfinity,
+             Double.PositiveInfinity,
+             false)
+
+  def checkCap(r: Ray, t: Double, y: Double): Boolean = {
+    val x: Double = (r.origin.x + r.direction.x * t)
+    val z: Double = (r.origin.z + r.direction.z * t)
+
+    (x * x + z * z) <= math.abs(y)
+  }
+}
---
+++ src/test/scala/raytracer/ConeTest.scala
@@ -0,0 +1,88 @@
+// Copyright (C) 2019 James McMurray
+//
+// raytracer_challenge is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// raytracer_challenge is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with raytracer_challenge  If not, see <http://www.gnu.org/licenses/>.
+
+package raytracer
+
+import org.scalatest.FunSuite
+
+class ConeTest extends FunSuite {
+  test("Cone.test_intersect1") {
+    val c: Cone    = Cone()
+    val r: Ray = Ray(Point(0, 0, -5), Vector(0,0,1).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 2 && doubleEq(xs(0).t, 5)  && doubleEq(xs(1).t, 5))
+  }
+  test("Cone.test_intersect2") {
+    val c: Cone    = Cone()
+    val r: Ray = Ray(Point(0, 0, -5), Vector(1,1,1).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 2 && doubleEq(xs(0).t, 8.66025)  && doubleEq(xs(1).t, 8.66025))
+  }
+  test("Cone.test_intersect3") {
+    val c: Cone    = Cone()
+    val r: Ray = Ray(Point(1, 1, -5), Vector(-0.5,-1,1).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 2 && doubleEq(xs(0).t, 4.5506)  && doubleEq(xs(1).t, 49.44994))
+  }
+  test("Cone.test_intersect_parallel") {
+    val c: Cone    = Cone()
+    val r: Ray = Ray(Point(0, 0, -1), Vector(0,1,1).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 1 && doubleEq(xs(0).t, 0.35355))
+  }
+  test("Cone.test_intersect_caps1") {
+    val c: Cone    = Cone().setMinimum(-0.5).setMaximum(0.5).setClosed(true)
+    val r: Ray = Ray(Point(0, 0, -5), Vector(0,1,0).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.isEmpty)
+  }
+  test("Cone.test_intersect_caps2") {
+    val c: Cone    = Cone().setMinimum(-0.5).setMaximum(0.5).setClosed(true)
+    val r: Ray = Ray(Point(0, 0, -0.25), Vector(0,1,1).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 2)
+  }
+  test("Cone.test_intersect_caps3") {
+    val c: Cone    = Cone().setMinimum(-0.5).setMaximum(0.5).setClosed(true)
+    val r: Ray = Ray(Point(0, 0, -0.25), Vector(0,1,0).normalise())
+
+    val xs: Seq[Intersection] = c.localIntersect(r)
+    assert(xs.length === 4)
+  }
+  test("Cone.test_normal1") {
+    val c: Cone    = Cone()
+    val n: RTTuple = c.localNormalAt(Point(0,0,0))
+
+    assert(n === Vector(0,0,0))
+  }
+  test("Cone.test_normal2") {
+    val c: Cone    = Cone()
+    val n: RTTuple = c.localNormalAt(Point(1,1,1))
+
+    assert(n === Vector(1,-math.sqrt(2),1))
+  }
+  test("Cone.test_normal3") {
+    val c: Cone    = Cone()
+    val n: RTTuple = c.localNormalAt(Point(-1,-1,0))
+
+    assert(n === Vector(-1,1,0))
+  }
+}
